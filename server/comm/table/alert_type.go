c52809e0e94b60a1a53bd2423f2ba86d53ffa414
qwganker <448081525@qq.com>
2025-12-13 09:15:30 +0800

    Bugfix: gorm time.time

--- server/comm/table/alert_type.go
+++ server/comm/table/alert_type.go
@@ -1,11 +1,11 @@
 package table
 
 type TAlertType struct {
-	ID        int64     `gorm:"column:id;primaryKey;autoIncrement"`
-	Name      string    `gorm:"column:name;not null;type=text"`
-	Code      string    `gorm:"column:code;not null;type=text"`
-	CreatedAt LocalTime `gorm:"column:created_at;autoCreateTime"`
-	UpdatedAt LocalTime `gorm:"column:updated_at;autoUpdateTime"`
+	ID        int64      `gorm:"column:id;primaryKey;autoIncrement"`
+	Name      string     `gorm:"column:name;not null;type=text"`
+	Code      string     `gorm:"column:code;not null;type=text"`
+	CreatedAt *LocalTime `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt *LocalTime `gorm:"column:updated_at;autoUpdateTime"`
 }
 
 func (TAlertType) TableName() string {
--- server/comm/table/prometheus.go
+++ server/comm/table/prometheus.go
@@ -1,17 +1,17 @@
 package table
 
 type TPrometheusConfig struct {
-	ID          int64     `gorm:"column:id;primaryKey;autoIncrement"`
-	Remark      string    `gorm:"column:remark;not null;type=text"`
-	Address     string    `gorm:"column:address;not null;type=text"`
-	Username    string    `gorm:"column:username;type=text"`
-	Password    string    `gorm:"column:password;type=text"`
-	CtrlAddress string    `gorm:"column:ctrl_address;not null;type=text"`
-	Config      string    `gorm:"column:config;type=text"`
-	Rule        string    `gorm:"column:rule;type=text"`
-	Enabled     string    `gorm:"column:enabled;;type:text"`
-	CreatedAt   LocalTime `gorm:"column:created_at;autoCreateTime"`
-	UpdatedAt   LocalTime `gorm:"column:updated_at;autoUpdateTime"`
+	ID          int64      `gorm:"column:id;primaryKey;autoIncrement"`
+	Remark      string     `gorm:"column:remark;not null;type=text"`
+	Address     string     `gorm:"column:address;not null;type=text"`
+	Username    string     `gorm:"column:username;type=text"`
+	Password    string     `gorm:"column:password;type=text"`
+	CtrlAddress string     `gorm:"column:ctrl_address;not null;type=text"`
+	Config      string     `gorm:"column:config;type=text"`
+	Rule        string     `gorm:"column:rule;type=text"`
+	Enabled     string     `gorm:"column:enabled;;type:text"`
+	CreatedAt   *LocalTime `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt   *LocalTime `gorm:"column:updated_at;autoUpdateTime"`
 }
 
 func (TPrometheusConfig) TableName() string {
